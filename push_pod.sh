4f83163fc03ba631f846277eb0100ee2400849bd
qixinbing <qixinbing@rongcloud.cn>
2021-04-26 15:35:59 +0800

    update push_pod.sh

--- push_pod.sh
+++ push_pod.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-# 统一归纳 SDK 的资源，统一管理资源文件
-
-target_path="Resources"
-
-if [ ! -d $target_path ];then
-	mkdir $target_path
-fi
-
-rsync -a IMKit/Resource/* $target_path/ && rm -rf IMKit/Resource/
-rsync -a Sticker/Resource/* $target_path/ && rm -rf Sticker/Resource/
-rsync -a iFlyKit/Resource/* $target_path/ && rm -rf iFlyKit/Resource/
-rsync -a CallKit/Resources/* $target_path/ && rm -rf CallKit/Resources/
-
-# 推 pod
-
 pod repo push Spec QRongIMSDK_UI.podspec --allow-warnings --use-libraries --verbose
